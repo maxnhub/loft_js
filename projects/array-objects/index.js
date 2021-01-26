3dd053973f297164b30187344f336abcec1e7f90
maxnhub <maxnikulnikov@gmail.com>
2021-01-26 23:35:22 +0300

    3 задачки

--- projects/array-objects/index.js
+++ projects/array-objects/index.js
@@ -9,7 +9,11 @@
  Пример:
    forEach([1, 2, 3], (el) => console.log(el))
  */
-function forEach(array, fn) {}
+function forEach(array, fn) {
+  for (let i = 0; i < array.length; i++) {
+    fn(array[i], i, array);
+  }
+}
 
 /*
  Задание 2:
@@ -20,7 +24,13 @@ function forEach(array, fn) {}
  Пример:
    map([1, 2, 3], (el) => el ** 2) // [1, 4, 9]
  */
-function map(array, fn) {}
+function map(array, fn) {
+  const newArr = [];
+  for (let i = 0; i < array.length; i++) {
+    newArr[i] = fn(array[i], i, array);
+  }
+  return newArr;
+}
 
 /*
  Задание 3:
@@ -41,7 +51,14 @@ function reduce(array, fn, initial) {}
  Пример:
    upperProps({ name: 'Сергей', lastName: 'Петров' }) вернет ['NAME', 'LASTNAME']
  */
-function upperProps(obj) {}
+function upperProps(obj) {
+  const array = [];
+  for (let key in obj) {
+    key = key.toUpperCase();
+    array.push(key);
+  }
+  return array;
+}
 
 /*
  Задание 5 *:
