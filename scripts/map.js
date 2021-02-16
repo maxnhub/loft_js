25a90d75b4915ee20050adaf91d67bd2de440b04
maxnhub <maxnikulnikov@gmail.com>
2021-02-16 18:42:27 +0300

    наброски vp_1

--- scripts/map.js
+++ scripts/map.js
@@ -7,8 +7,8 @@ let placemarks = [
             hintContent: '<div class="map__hint">ул.Литераторов д.19</div>',
             balloonContent: [
                 '<div class="map__balloon">',
-                '<img class="map__burger-img" src="../src/images/steam.png" alt="Бургер"/>',
-                'Самые вкусные бургеры у нас! Заходите по адресу: ул.Литераторов д.19',
+                '<img class="map__burger-img" src="./src/icons/google-map-marker.png" alt="Бургер"/>',
+                'Пышечная по адресу: ул.Литераторов д.19',
                 '</div>'
             ]
         },
@@ -18,8 +18,8 @@ let placemarks = [
             hintContent: '<div class="map__hint">Малый пр-т В.О. д.64</div>',
             balloonContent: [
                 '<div class="map__balloon">',
-                '<img class="map__burger-img" src="../src/images/steam.png" alt="Бургер"/>',
-                'Самые вкусные бургеры у нас! Заходите по адресу: Малый пр-т В.О. д.64',
+                '<img class="map__burger-img" src="./src/icons/google-map-marker.png" alt="Бургер"/>',
+                'Наверняка какой-нибудь музей: Малый пр-т В.О. д.64',
                 '</div>'
             ]
         },
@@ -29,8 +29,8 @@ let placemarks = [
             hintContent: '<div class="map__hint">наб. реки Фрнтанки д.56</div>',
             balloonContent: [
                 '<div class="map__balloon">',
-                '<img class="map__burger-img" src="../src/images/steam.png" alt="Бургер"/>',
-                'Самые вкусные бургеры у нас! Заходите по адресу: наб. реки Фонтанки д.56',
+                '<img class="map__burger-img" src="./src/icons/google-map-marker.png" alt="Бургер"/>',
+                'Рандомные мужики ловят рыбу: наб. реки Фонтанки д.56',
                 '</div>'
             ]
         }
@@ -38,7 +38,7 @@ let placemarks = [
     geoObjects = [];
 
 function init() {
-    var map = new ymaps.Map('map', {
+    var myMap = new ymaps.Map('map', {
         center: [59.94, 30.32],
         zoom: 12,
         controls: ['zoomControl'],
@@ -52,8 +52,8 @@ function init() {
             },
             {
                 iconLayout: 'default#image',
-                iconImageHref: '../src/icons/map-marker.png',
-                iconImageSize: [46, 57],
+                iconImageHref: './src/icons/google-map-marker.png',
+                iconImageSize: [37, 55],
                 iconImageOffset: [-23, -57]
             });
 
@@ -61,13 +61,49 @@ function init() {
     let clusterer = new ymaps.Clusterer({
         clusterIcons: [
             {
-                href: '../src/images/steam.png',
-                size: [100, 100],
-                offset: [-50, -50]
+                href: './src/icons/cbaner_pin.png',
+                size: [60, 60],
+                offset: [-20, -50]
             }
         ],
         clusterIconContentLayout: null
     });
-    map.geoObjects.add(clusterer);
+    myMap.events.add('click', function (e) {
+        var coords = e.get('coords');
+        var placemark = new YMaps.Placemark(new YMaps.GeoPoint(coords[0], coords[1]), {draggable: 1,
+            hintOptions: {
+                maxWidth: 100,
+                showTimeout: 200,
+                offset: new YMaps.Point(5, 5)
+            },
+            balloonOptions: {
+                maxWidth: 70,
+                hasCloseButton: false,
+                mapAutoPan: 0
+            }
+        });
+        placemark.name = "Имя метки";
+        placemark.description = "Описание метки";
+        myMap.addOverlay(placemark);
+        placemark.openBalloon();
+
+        // if (!myMap.balloon.isOpen()) {
+        //     var coords = e.get('coords');
+        //     myMap.balloon.open(coords, {
+        //         contentHeader:'Событие!',
+        //         contentBody:'<p>Кто-то щелкнул по карте.</p>' +
+        //             '<p>Координаты щелчка: ' + [
+        //                 coords[0].toPrecision(6),
+        //                 coords[1].toPrecision(6)
+        //             ].join(', ') + '</p>',
+        //         contentFooter:'<sup>Щелкните еще раз</sup>'
+        //     });
+        // }
+        // else {
+        //     myMap.balloon.close();
+        // }
+    });
+    myMap.geoObjects.add(clusterer);
+    myMap.behaviors.enable('scrollZoom')
     clusterer.add(geoObjects);
 }
