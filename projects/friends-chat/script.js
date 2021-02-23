73e59cd360e9f0bb45b8a33ab0f2be9413510820
maxnhub <maxnikulnikov@gmail.com>
2021-02-23 23:33:08 +0300

    'something'

---
+++ projects/friends-chat/script.js
@@ -0,0 +1,37 @@
+VK.init({
+    apiId: 7770956
+});
+
+function auth() {
+    return new Promise((resolve, reject) => {
+        VK.Auth.login(data => {
+            if (data.session) {
+                resolve();
+            } else {
+                reject(new Error('Не удалось авторизоваться'));
+            }
+        }, 2);
+    });
+}
+
+function callAPI(method, params) {
+    params.v = '5.76';
+
+    return new Promise((resolve, reject) => {
+        VK.api(method, params, (data) => {
+            if (data.error) {
+                reject(data.error);
+            } else {
+                resolve(data.response);
+            }
+        });
+    });
+}
+
+auth()
+    .then(() => {
+    return callAPI('users.get', { name_case: 'gen'});
+    })
+    .then(me => {
+        console.log(me);
+    })
